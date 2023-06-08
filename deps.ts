32b2b554acb591c65a338ba77dc1378de2891f92
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-07 20:35:32 -0700

    removed extra deps

--- deps.ts
+++ deps.ts
@@ -1,8 +1,4 @@
 export * as dotenv from "https://deno.land/std@0.188.0/dotenv/mod.ts";
 export * as discord from "https://deno.land/x/discord_api_types@0.37.42/v10.ts";
 export { Ngrok } from "https://deno.land/x/ngrok@4.0.1/mod.ts";
-export type { GitHubCodemodBuilderCreatePROptions } from "https://deno.land/x/codemod@v0.0.3/github/mod.ts";
-export { GitHubCodemodBuilder } from "https://deno.land/x/codemod@v0.0.3/github/mod.ts";
-export type { GitHubAPIClientOptions } from "https://deno.land/x/codemod@v0.0.3/github/api/mod.ts";
-export type { Officer } from "https://acmcsuf.com/code/raw/65d3cac9653f4469f51a4dad224d8878795ffc55/src/lib/public/board/types.ts";
 export { default as nacl } from "https://esm.sh/tweetnacl@1.0.3";
